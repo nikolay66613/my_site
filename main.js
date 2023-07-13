1924f9a5dcae07e19ea44e2c4b369011d862baf3
NKolyaskin <nikolay66613@74.ru>
2023-07-13 13:12:38 +0300

    добавлено мобильное меню

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const btnMobileMenu = document.querySelector(".mobile_icon");
 const mobileMenu = document.querySelector('.wrapper_header');
+const mobileMenuContent = document.querySelector('.cont_header');
+const mobileMenuNav = document.querySelector('.nav');
+
+const mobileMenuNavItem = document.querySelector('.list_menu');
+
 
 
 
 btnMobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('.activ_menu');
+    mobileMenu.classList.toggle('show_menu');
+    mobileMenuContent.classList.toggle('show_cont_mobile');
+    mobileMenuNav.classList.toggle('show_cont_mobile');
+})
+
+mobileMenuNavItem.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show_menu');
+    mobileMenuContent.classList.toggle('show_cont_mobile');
+    mobileMenuNav.classList.toggle('show_cont_mobile');
 })
