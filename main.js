afa951360bfe3b5f43a95ada7c7fdc1dd452c456
NKolyaskin <nikolay66613@74.ru>
2023-07-13 18:04:46 +0300

    Добавлено мобильное меню и сладер в проекты на мобилку

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btnMobileMenu = document.querySelector(".mobile_icon");
+const btnMobileMenu = document.querySelector(".btn_menu");
 const mobileMenu = document.querySelector('.wrapper_header');
 const mobileMenuContent = document.querySelector('.cont_header');
 const mobileMenuNav = document.querySelector('.nav');
@@ -15,7 +15,50 @@ btnMobileMenu.addEventListener('click', () => {
 })
 
 mobileMenuNavItem.addEventListener('click', () => {
-    mobileMenu.classList.toggle('show_menu');
-    mobileMenuContent.classList.toggle('show_cont_mobile');
-    mobileMenuNav.classList.toggle('show_cont_mobile');
+    if(window.innerWidth <= 960){
+        mobileMenu.classList.toggle('show_menu');
+        mobileMenuContent.classList.toggle('show_cont_mobile');
+        mobileMenuNav.classList.toggle('show_cont_mobile');
+    }
 })
+
+// slider
+
+const slider = document.querySelector('.project_list');
+const itemSlider = Array.from(slider.children);
+
+const btnPrevSlider = document.querySelector('.btn_left');
+const btnNextSlider = document.querySelector('.btn_right');
+
+let activeSlide = 0;
+
+itemSlider[activeSlide].classList.add('show_slider');
+
+
+btnNextSlider.addEventListener('click', () => {
+    if(activeSlide < itemSlider.length - 1) {
+        itemSlider[activeSlide].classList.remove('show_slider');
+        activeSlide++;
+        itemSlider[activeSlide].classList.add('show_slider');
+    } else {
+        itemSlider[activeSlide].classList.remove('show_slider');
+        activeSlide = 0;
+        itemSlider[activeSlide].classList.add('show_slider');
+    }
+})
+
+btnPrevSlider.addEventListener('click', () => {
+    if(activeSlide < itemSlider.length - 1) {
+        itemSlider[activeSlide].classList.remove('show_slider');
+        ++activeSlide;
+        itemSlider[activeSlide].classList.add('show_slider');
+    } else {
+        itemSlider[activeSlide].classList.remove('show_slider');
+        activeSlide = 0;
+        itemSlider[activeSlide].classList.add('show_slider');
+    }
+})
+
+
+
+
